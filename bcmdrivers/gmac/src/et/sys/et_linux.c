334cf73f0a9cc65bacb3bf5e4ea0b97af9234e4d
li_hongzhen <li_hongzhen@163.com>
2019-01-24 09:52:09 +0800

    使能网卡的混杂模式

--- bcmdrivers/gmac/src/et/sys/et_linux.c
+++ bcmdrivers/gmac/src/et/sys/et_linux.c
@@ -1618,8 +1618,7 @@ et_set_multicast_list(struct net_device *dev)
 		etc->nmulticast = i;
 
 		/* LR: partial re-init, DMA is already initialized */
-/*1/0/1 down when get state delete by zhangjiajie 2017-6-9*/
-//		et_init(et, ET_INIT_INTRON); 
+		et_init(et, ET_INIT_INTRON); 
 	}
 
 	if (locked) {
